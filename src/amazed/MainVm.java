bb9ec8f69c56cdba863053ea92e50342a356416d
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-10-16 15:09:53 +0200

    MainVm addded no more run arguments

---
+++ src/amazed/MainVm.java
@@ -0,0 +1,10 @@
+package amazed;
+
+public class MainVm {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		amazed.Main.main(new String[] {"./maps/medium.map", "parallel-9", "500"});
+	}
+
+}
